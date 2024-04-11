3d244a2dd8584e85f4d2289c4a2c884b21ad62f4
SarahJuulChristiansen <166373082+SarahJuulChristiansen@users.noreply.github.com>
2024-04-11 09:01:01 +0200

    Add files via upload

--- javascript/dropdown.js
+++ javascript/dropdown.js
@@ -1,8 +1,8 @@
 // Funktion til at håndtere klik på dropdown-knappen
 function dropdownKategori() {
-  var dropdown = document.getElementById("myDropdown");
+  let dropdown = document.getElementById("myDropdown");
   dropdown.classList.toggle("show");
-  var dropdownButton = document.getElementById('dropdownButton');
+  let dropdownButton = document.getElementById('dropdownButton');
   if (dropdown.classList.contains("show")) {
       dropdownButton.style.backgroundColor = "#D5802D"; // Ændrer baggrundsfarven til orange
   } else {
@@ -12,10 +12,10 @@ function dropdownKategori() {
 
 // Funktion til at håndtere valg af kategori og opdatering af knaptekst samt lukning af dropdown
 function valgAfKategori(kategoriNavn) {
-  var dropdownButton = document.getElementById('dropdownButton');
+  let dropdownButton = document.getElementById('dropdownButton');
   dropdownButton.innerHTML = kategoriNavn; // Opdaterer knapteksten med navnet på den valgte kategori
   dropdownButton.style.backgroundColor = "#ffffff"; // Ændrer baggrundsfarven til hvid, når en kategori er valgt
-  var dropdown = document.getElementById("myDropdown");
+  let dropdown = document.getElementById("myDropdown");
   dropdown.classList.remove("show"); // Lukker dropdown'en
 }
 
